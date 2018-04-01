1d2c4bf20d6d762fc5598c15ac0e92ce96dc59e4
Luiz Felipe <lflimeira02@gmail.com>
2018-04-01 20:49:03 -0300

    hello world commit with test

---
+++ tests/main.spec.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect
+const exec = require('child_process').exec
+const btcConverter = './src/main.js'
+
+describe('Main CLI', () => {
+  it('should return Hello World', (done) => {
+    exec(btcConverter, (err, stdout, stderr) => {
+      if(err) throw err
+      expect(stdout.replace('\n','')).to.be.equal('Hello World!')
+      done()
+    })
+  })
+})
