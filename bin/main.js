3ce320193cb6e0fd1b40e39dee150832fe51005e
Luiz Felipe <lflimeira02@gmail.com>
2018-04-07 12:43:13 -0300

    cli: Add amount and currency option in the CLI

--- bin/main.js
+++ bin/main.js
@@ -3,5 +3,8 @@
 
 const program = require('commander');
 const pkg = require('../package.json');
+const convertBTC = require('./ConvertBTC');
 
-program.version(pkg.version).description('Convert Bitcoin to any currency defined.').parse(process.argv);
+program.version(pkg.version).description('Convert Bitcoin to any currency defined.').option('-C, --currency <currency>', 'Currency to be converted. (Default: USD)').option('-A, --amount <amount>', 'Value in Bitcoin to convert. (Default: 1)').parse(process.argv);
+
+console.log(convertBTC(program.currency, program.amount));
