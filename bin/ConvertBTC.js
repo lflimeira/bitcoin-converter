7182ed2c3a5db3a5686c8d0b7058d1f685b9deda
Luiz Felipe <lflimeira02@gmail.com>
2018-04-22 00:43:07 -0300

    bin: update de project's bin

--- bin/ConvertBTC.js
+++ bin/ConvertBTC.js
@@ -1,5 +1,21 @@
+const chalk = require('chalk');
+const request = require('request');
+
 function convertBTC(currency = 'USD', amount = 1) {
-  return `${amount} BTC to ${currency} = 2000.00`;
+  const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
+
+  request(url, (error, response, body) => {
+    let apiResponse;
+
+    try {
+      apiResponse = JSON.parse(body);
+    } catch (parseError) {
+      console.log(chalk.red('Something went wrong in the API. Try in a few minutes.'));
+      return parseError;
+    }
+
+    console.log(`${chalk.red(amount)} BTC to ${chalk.cyan(currency)} = ${chalk.yellow(apiResponse.price)}`);
+  });
 }
 
 module.exports = convertBTC;
